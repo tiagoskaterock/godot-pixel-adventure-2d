d96417f6447254a82093c57e66b2598a8b557cd2
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-03 06:45:45 -0300

    Animações aplicadas quando parado ou correndo

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,6 +9,7 @@ var move_direction
 func _physics_process(delta):
 	check_move_direction()
 	check_jump()
+	check_animation()	
 	move(delta)
 	
 
@@ -16,6 +17,11 @@ func check_move_direction():
 	move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	if move_direction < 0: $Sprite.flip_h = true		
 	elif move_direction > 0: $Sprite.flip_h = false
+	
+
+func check_animation():
+	if move_direction != 0 : $AnimationPlayer.play("run")
+	else: $AnimationPlayer.play("idle")
 	
 	
 func check_jump():
