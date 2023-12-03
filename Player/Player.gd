47996590efd9d8e84dac0593581f6e073a74baac
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-03 00:55:44 -0300

    #03 - Tutorial Godot - Jogo de Plataforma 2D - Tilemap e Colisões --> aula aqui --> https://www.youtube.com/watch?v=VlCXOMzicSg&list=PL-oJEh-N3A3Qis2H0Mi-_jaq1c5oFd2Ty&index=3

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,30 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var move_speed = 480
+var move_speed = 200
 var gravity = 1200
 var jump_force = 300
+var move_direction
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	check_move_direction()
+	check_jump()
+	move(delta)
+	
+
+func check_move_direction():
+	move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	if move_direction < 0: $Sprite.flip_h = true		
+	elif move_direction > 0: $Sprite.flip_h = false
+	
+	
+func check_jump():
 	if Input.is_action_pressed("jump"):
 		velocity.y = -jump_force
-	velocity.x = move_speed * move_direction
+		
+		
+func move(delta):
+	velocity.y += gravity * delta	
+	velocity.x = move_speed * move_direction	
 	move_and_slide(velocity)
-	
+		
