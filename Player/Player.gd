fb931110f9a04e2415194fb5cc687740f83d476d
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-04 17:39:49 -0300

    https://www.youtube.com/watch?v=r28Ovl8qPlA&list=PL-oJEh-N3A3Qis2H0Mi-_jaq1c5oFd2Ty&index=8 - #08 - Tutorial Godot - Jogo de Plataforma 2D - Itens Coletáveis

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,19 +3,26 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 var move_speed = 200
 var gravity = 1200
-var jump_force = 350
+var jump_force = 450
 var move_direction
+var terminal_velocity = 500
 onready var raycasts = $RaycastsGroup
 
 
-func _physics_process(delta):
+func _physics_process(delta):	
+	if is_player_on_ground(): velocity.y = 0
 	check_move_direction()
 	check_jump()
-	check_animation()	
+	check_animation()		
+	check_terminal_velocity()
 	move(delta)
 	
-
-func check_move_direction():
+	
+func check_terminal_velocity():
+	if velocity.y > terminal_velocity: velocity.y = terminal_velocity
+	
+	
+func check_move_direction():		
 	move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))	
 	if move_direction < 0: $Sprite.flip_h = true		
 	elif move_direction > 0: $Sprite.flip_h = false
@@ -36,7 +43,7 @@ func move(delta):
 	velocity.y += gravity * delta	
 	# velocity.x = move_speed * move_direction	
 	
-	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)	
 	move_and_slide(velocity)
 		
 
