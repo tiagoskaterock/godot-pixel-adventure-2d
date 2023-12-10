c553105dc47217c34ddf95028288e8637acaaf9e
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-12-10 10:42:48 -0300

    https://www.youtube.com/watch?v=UkwdipBVLJ8&list=PL-oJEh-N3A3Qis2H0Mi-_jaq1c5oFd2Ty&index=11 - #11 - Tutorial Godot - Jogo de Plataforma 2D - Criando uma Plataforma que cai(caindo)

--- Levels/FallingPlatform/FallingPlatform.gd
+++ Levels/FallingPlatform/FallingPlatform.gd
@@ -2,9 +2,11 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var gravity = 0
+var gravity_to_fall = 300
+onready var reset_position = global_position.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta):	
 	velocity.y += gravity * delta
 	move_and_slide(velocity, Vector2.UP) 
 
@@ -16,4 +18,11 @@ func _on_AreaDetection_body_entered(body):
 
 
 func _on_TimerToFall_timeout():
-	gravity = 650
+	$TimerToRespawn.start()
+	gravity = gravity_to_fall
+
+
+func _on_TimerToRespawn_timeout():
+	global_position.y = reset_position
+	gravity = 0
+	velocity.y = 0
